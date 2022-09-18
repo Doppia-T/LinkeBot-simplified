789bca5e73b0dbb9844b7abdabc2aacbc5c4cc24
Doppia-T <64402378+Doppia-T@users.noreply.github.com>
2022-09-18 18:14:25 +0200

    Update simplified_core.py

--- simplified_core.py
+++ simplified_core.py
@@ -11,3 +11,39 @@ import os
 import sys
 import time
 
+
+# gets username from an external file called "LinCred"
+# quite raw method of getting the credential from a .txt file
+def get_LinkeID():
+    with open(os.path.join(sys.path[0], "LinCred.txt"), "r") as find_cred:
+        for line in find_cred:
+            if line.startswith('id'):
+                id_line = line
+                spl_word = 'id = '
+                spl_id_line = id_line.split(spl_word,1)[1]
+                LinkeUsername = spl_id_line
+                return LinkeUsername
+
+# gets password from an external file called "LinCred"
+# quite raw method of getting the credential from a .txt file
+def get_LinkePSS():
+    with open(os.path.join(sys.path[0], "LinCred.txt"), "r") as find_cred:
+        for line in find_cred:
+            if line.startswith('password'):
+                pss_line = line
+                spl_word = 'password = '
+                spl_pss_line = pss_line.split(spl_word,1)[1]
+                LinkePassword = spl_pss_line
+                return LinkePassword
+
+# gets target profile from an external file called "LinTarg" quite raw method of getting the target 
+# from a .txt file
+def get_LinkeTGT():
+    with open(os.path.join(sys.path[0], "LinTarg.txt"), "r") as find_targ:
+        for line in find_targ:
+            if line.startswith('profile'):
+                tgt_line = line
+                spl_word = 'profile = '
+                spl_tgt_line = tgt_line.split(spl_word,1)[1]
+                LinkeTarget = spl_tgt_line
+                return LinkeTarget
