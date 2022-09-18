427b2f162c7841221146542ec4c6774d9a339bc0
Doppia-T <64402378+Doppia-T@users.noreply.github.com>
2022-09-18 18:13:52 +0200

    Create simplified_core.py

---
+++ simplified_core.py
@@ -0,0 +1,13 @@
+# gets libraries needed
+from xml.dom.minidom import Element
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import os
+import sys
+import time
+
